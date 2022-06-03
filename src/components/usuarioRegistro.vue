dc23b492a2b009384148fdeff9f1f8f7bb5e3ec8
mora <moraluis718@gmail.com>
2022-06-02 19:45:27 -0500

    se estructuro mejor la informacion de usuarios y se agregaron nuevas librerias @luisMora

--- src/components/usuarioRegistro.vue
+++ src/components/usuarioRegistro.vue
@@ -1,140 +1,182 @@
 <template>
   <v-row justify="center">
-    <v-col
-      cols="12"
-      sm="10"
-      md="8"
-      lg="6"
-    >
-      <v-card ref="form" id="form" name="form" v-on:submit.prevent="procesar();">
+    <v-col cols="12" sm="10" md="8" lg="6">
+      <v-card>
         <v-card-text>
+          <v-form ref="formAdd">
             <center>
               <h1>Registrarse</h1>
-              <br>
-          </center>
-          <v-text-field
-            ref="nombre"
-            value=""
-            v-model="nombre"
-            :rules="[rules.required]"
-            :error-messages="errorMessages"
-            label="Nombre Usuario"
-            persistent-hint
-            outlined
-          ></v-text-field>
-          <v-text-field
-            persistent-hint
-            outlined
-            ref="email"
-            v-model="email"
-            value=""
-            :rules="[
-              () => /.+@.+\..+/.test(email) || 'E-mail debe de ser valido', 
-              rules.required,
-            ]"
-            label="E-mail"
-            required
-          ></v-text-field>
-          <v-text-field
-            persistent-hint
-            outlined
-            v-model="contraseña"
-            :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-            :rules="[rules.required, rules.min]"
-            :type="show1 ? 'text' : 'password'"
-            name="contraseña"
-            value=""
-            label="Contraseña"
-            @click:append="show1 = !show1"
-          ></v-text-field>
-          <v-text-field
-            persistent-hint
-            outlined
-            v-model="verificarC"
-            :rules="[rules.required]"
-            :type="show1 ? 'text' : 'password'"
-            name="verificarC"
-            label="Confirmar Contraseña"
-            value=""
-          ></v-text-field>
+              <br />
+            </center>
+            <v-text-field
+              ref="nombre"
+              value=""
+              v-model="editedItem.nombre"
+              :rules="rulesNombre"
+              :error-messages="errorMessages"
+              label="Nombre Usuario"
+              persistent-hint
+              outlined
+            ></v-text-field>
+            <v-text-field
+              persistent-hint
+              outlined
+              ref="email"
+              v-model="editedItem.email"
+              :rules="rulesEmail"
+              label="E-mail"
+              required
+            ></v-text-field>
+            <v-text-field
+              persistent-hint
+              outlined
+              v-model="editedItem.password"
+              :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+              :rules="rulesPass"
+              :type="show1 ? 'text' : 'password'"
+              name="contraseña"
+              value=""
+              label="Contraseña"
+              @click:append="show1 = !show1"
+            ></v-text-field>
+            <v-text-field
+              persistent-hint
+              outlined
+              v-model="editedItem.verificarC"
+              :rules="rulesPassC"
+              :type="show1 ? 'text' : 'password'"
+              name="verificarC"
+              label="Confirmar Contraseña"
+              value=""
+            ></v-text-field>
+          </v-form>
         </v-card-text>
         <v-divider class="mt-12"></v-divider>
         <v-card-actions>
-          <v-btn
-            class="mr-4"
-            color="primary"
-            text
-            @click="submit"
-          >
+          <v-btn class="mr-4" color="primary" text @click="save">
             REGISTRARME
           </v-btn>
         </v-card-actions>
       </v-card>
     </v-col>
   </v-row>
-
 </template>
 
 <script>
+// import axios from "axios"
+import { mapActions, mapGetters, mapState } from "vuex";
+import Swal from "sweetalert2";
 
-import axios from "axios"
+export default {
+  data() {
+    return {
+      //Declarar arreglo de documento Json
+      usuario: [],
+      editedItem: {
+        nombre: "",
+        email: "",
+        password: "",
+        verificarC: "",
+      },
+      errorMessages: "",
+      //Variables de visualizacion de contraseñas
+      show1: false,
+      show2: false,
+      //Validaciones de formulario
+      rulesNombre: [
+        (value) => !!value || "Campo Necesario",
+        (v) => v.length > 3 || "Min 4 caracteres",
+      ],
+      rulesEmail: [
+        (value) => !!value || "Campo Necesario",
+        (v) =>
+          /^(([^<>()[\]\\.,;:\s@']+(\.[^<>()\\[\]\\.,;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+            v
+          ) || "E-mail no es valido",
+      ],
+      rulesPass: [
+        (value) => !!value || "Campo Necesario",
+        (v) => v.length > 7 || "Min 8 caracteres",
+      ],
+      rulesPassC: [
+        (value) => !!value || "Campo Necesario",
+        (v) => v.length > 7 || "Min 8 caracteres",
+      ],
+    };
+  },
 
-  export default {
-    data () {
+  computed: {
+    // ESTO NO SE SI VA
+    form() {
       return {
-        //Declarar arreglo de documento Json
-        usuario: [],
-        errorMessages: '',
-        //Iniacilizar variables de formulario
-        nombre: null,
-        email: null,
-        contraseña: null,
-        verificarC: null,
-        //Variables de visualizacion de contraseñas
-        show1: false,
-        show2: false,
-        //Validaciones de formulario
-        rules: {
-          required: value => !!value || 'Campo Necesario',
-          min: v => v.length >= 8 || 'Min 8 caracteres',
-          emailVerificar: ()=>  (`The email and password you entered don't match`),
-        },
+        nombre: this.nombre,
+        email: this.email,
+        contraseña: this.contraseña,
+        verificarC: this.verificarC,
+      };
+    },
+    ...mapState("usuarios", ["status", "errorMessage"]),
+  },
+
+  watch: {
+    name() {
+      this.errorMessages = "";
+    },
+  },
+
+  methods: {
+    ...mapActions("usuarios", ["AddUsuario"]),
+
+    save() {
+      // se valida si el formulario esta bien
+      if (this.$refs.formAdd.validate()) {
+        // se manda el modelo
+        this.AddUsuario(this.editedItem).then((respuesta) => {
+          if (!this.status) {
+            Swal.fire({
+              icon: "success",
+              title: "Usuario registrado",
+              showConfirmButton: true,
+              // timer: 1500
+            });
+          } else {
+            Swal.fire({
+              type: "error",
+              title: "Oops...",
+              text: this.errorMessage,
+            });
+          }
+        });
+      } else {
+        this.validationError();
       }
     },
-
-    computed: {
-      // ESTO NO SE SI VA
-      form () {
-        return {
-          nombre: this.nombre,
-          email: this.email,
-          contraseña: this.contraseña,
-          verificarC: this.verificarC,
-        }
-      },
+    // metodo para mandar error en el formulario
+    validationError() {
+      Swal.fire({
+        icon: "warning",
+        title: "Estas idiota o que!",
+        text: "hay campos sin llenar",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+      });
     },
 
-    watch: {
-      name () {
-        this.errorMessages = ''
-      },
-    },
-
-    methods: {
-      //ESTE METODO ME LO PASO TOÑITO PERO NO SALE
-      async submit(){
-        const res = await axios.post(`http://localhost:3000/usuarios`,{
-          nombre: this.nombre,
-          email: this.email,
-          contraseña: this.contraseña,
-          verificarC: this.verificarC,
-        })
-        this.usuario = [...this.usuario,res.data];
-        this.nombre = "";
-        this.email= "";
-        this.contraseña= "";
-        this.verificarC= "";
-      }
-    },
-  }
+    //ESTE METODO ME LO PASO TOÑITO PERO NO SALE
+    // Si no sale pa que vrgs lo pones mamon
+    // async submit(){
+    //   const res = await axios.post(`http://localhost:3000/usuarios`,{
+    //     nombre: this.nombre,
+    //     email: this.email,
+    //     contraseña: this.contraseña,
+    //     verificarC: this.verificarC,
+    //   })
+    //   this.usuario = [...this.usuario,res.data];
+    //   this.nombre = "";
+    //   this.email= "";
+    //   this.contraseña= "";
+    //   this.verificarC= "";
+    // }
+  },
+};
 </script>
